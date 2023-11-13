5a5e365bf4ccf38d9ba0db3ec7b4e1f5e5c38025
HashimRasheed <hashimrasheed1999@gmail.com>
2023-11-13 15:13:12 +0530

    Project initializing

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
